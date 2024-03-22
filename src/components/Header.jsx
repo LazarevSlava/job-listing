a3cbea10849b01d5b9bdb77fdd047371ccf4d3d3
LazarevSlava <549132w@gmail.com>
2024-03-22 10:06:21 +0100

    Created header and main

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+import style from './Header.module.scss';
+
+function Header() {
+  return <div className={style.head}></div>;
+}
+
+export { Header };
---
+++ src/components/Main.jsx
@@ -0,0 +1,7 @@
+import style from './Main.module.scss';
+
+function Main() {
+  return <div className={style.back}></div>;
+}
+
+export { Main };
